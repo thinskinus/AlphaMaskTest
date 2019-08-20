49517464b01c9ed042c694fc160b06c6704316b4
Elena Demidova <elena.demidova@firstlinesoftware.com>
2019-08-20 10:37:40 +0300

    Initial Commit

---
+++ AlphaMaskTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlphaMaskTest
+//
+//  Created by Elena Demidova on 20/08/2019.
+//  Copyright © 2019 Elena Demidova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
